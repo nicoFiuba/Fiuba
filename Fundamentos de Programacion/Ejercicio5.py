4b6849f0812380432d6fe88519d1cabf04b354f2
Nico <nicolas.a.fel@gmail.com>
2025-04-07 23:49:36 -0300

    Se sigue avanzando en Fundamentos

--- Fundamentos de Programacion/Ejercicio5.py
+++ Fundamentos de Programacion/Ejercicio5.py
@@ -8,3 +8,13 @@ print(f"El diámetro es: {diametro}.\nSu perímetro es: {perimetro}.\nSu superfi
 #2) Solicitar el ingreso de tres palabras y luego mostrar todas las frases posibles, resultantes de combinar las 3 palabras ingresadas.
 palabras=input("Ingrese tres palabras separadas por un espacio: ").split() #split() separa las palabras por el espacio
 print(f"Las combinaciones posibles son: \n{palabras[0]} {palabras[1]} {palabras[2]}\n{palabras[0]} {palabras[2]} {palabras[1]}\n{palabras[1]} {palabras[0]} {palabras[2]}\n{palabras[1]} {palabras[2]} {palabras[0]}\n{palabras[2]} {palabras[0]} {palabras[1]}\n{palabras[2]} {palabras[1]} {palabras[0]}")
+
+"""
+Ejercicio 2 de manera optimizada (usa librería itertools para generar combinaciones de forma automatica):
+
+import itertools
+palabras=input("Ingrese tres palabras separadas por un espacio: ").split() #split() separa las palabras por el espacio
+combinaciones=itertools.permutations(palabras) #Genera todas las combinaciones posibles de las palabras ingresadas
+print ("Las combinaciones posibles son:")
+for combinacion in combinaciones:
+"""
