bdb4667f64613dbcd268e4f7f2ecc7674bc3c65e
nicoFiuba <nfeldman@fi.uba.ar>
2025-05-04 21:31:35 +0000

    Avanzando en IDS

---
+++ Introduccion al Desarollo de Software/Clase10/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/gallery')
+def gallery():
+    return render_template("gallery.html")
+
+@app.route('/menu')
+def menu():
+    return render_template("menu.html")
+
+@app.route('/reservation')
+def reservation():
+    return render_template("reservation.html")
+
+
+if __name__ == '__main__':
+   app.run(host="localhost", port=8080, debug=True)
+
