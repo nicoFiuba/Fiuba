5de3bc1c7f093ddd4bcabac27ed2ded87d858105
nicoFiuba <nfeldman@fi.uba.ar>
2025-05-08 18:40:38 +0000

    Clase 10 terminada

--- Introduccion al Desarollo de Software/Clase10/app.py
+++ Introduccion al Desarollo de Software/Clase10/app.py
@@ -1,30 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = 'hola'
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    usuario = "Nico"
+    flash(f"Bienvenido {usuario}", "info") #metodos de flash: success, info, warning, error
+    return render_template("index.html", active_page='index')
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", active_page='about')
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", active_page='contact')
 
 @app.route('/gallery')
 def gallery():
-    return render_template("gallery.html")
+    return render_template("gallery.html", active_page='gallery')
 
 @app.route('/menu')
 def menu():
-    return render_template("menu.html")
+    return render_template("menu.html", active_page='menu')
 
 @app.route('/reservation')
 def reservation():
-    return render_template("reservation.html")
+    return render_template("reservation.html", active_page='reservation')
+
+@app.route('/reserva', methods=['GET', 'POST'])
+def reserva():
+    if request.method == 'POST':
+        flash("Reserva realizada con éxito", "success")
+        return redirect(url_for('reserva'))
+        
+    return render_template("reservation.html", active_page='reservation')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
 if __name__ == '__main__':
