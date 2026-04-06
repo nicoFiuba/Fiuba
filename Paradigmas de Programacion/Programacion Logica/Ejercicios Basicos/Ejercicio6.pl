086dfe65d062b617789a9d390f7b356fc6977594
nicoFiuba <nfeldman@fi.uba.ar>
2026-04-06 16:37:51 -0300

    Basicos y arboles finalizados

--- Paradigmas de Programacion/Programacion Logica/Ejercicios Basicos/Ejercicio6.pl
+++ Paradigmas de Programacion/Programacion Logica/Ejercicios Basicos/Ejercicio6.pl
@@ -15,8 +15,11 @@ byPlane(bangkok,auckland).
 byPlane(singapore,auckland).
 byPlane(losAngeles,auckland).
 
-travel(X,Y) :- byCar(X,Y).
-travel(X, Y) :- byTrain(X,Y).
-travel(X, Y) :- byPlane(X,Y).
+% Para no estar llamando a la base de datos cada vez, se puede crear una regla que unifique los tres medios de transporte.
+sinEscalas(X,Y) :- byCar(X,Y).
+sinEscalas(X, Y) :- byTrain(X,Y).
+sinEscalas(X, Y) :- byPlane(X,Y).
 
-% FALTA DEFINIR travel(X,Y) PARA QUE INCLUYA LOS VIAJES CON ESCALAS
+% Armo el viaje con o sin escalas. Si no hay escalas, se llama a la regla sinEscalas. Si hay escalas, se llama a la regla sinEscalas para el primer tramo del viaje y luego se llama a la regla travel para el resto del viaje.
+travel(X, Y) :- sinEscalas(X, Y). % Caso base: si no hay escalas, el viaje es directo.
+travel(X, Y) :- sinEscalas(X, Z), travel(Z, Y). % Caso recursivo: si hay escalas, se busca un punto intermedio Z y se llama a travel para el resto del viaje, pueden haber infinidad de escalas, por eso se llama a travel y no a sinEscalas.
